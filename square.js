934476af923d939c449fbd1d96cee528dc9289bd
Deve Tambat <54927248+tambatd@users.noreply.github.com>
2021-04-03 14:44:08 -0500

    Update square.js

--- square.js
+++ square.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(1010, 1000);
+  createCanvas(11000, 17000);
   noStroke();
   for(i=20;i<975;i+=150){
   for(j=20;j<975;j+=50){
@@ -19,8 +19,8 @@ function draw(){
 
 function touchStarted(){
   background(random(255), random(255), random(255));
-  for(i=20;i<975;i+=25){
-  for(j=20;j<975;j+=20){
+  for(i=20;i<17000;i+=25){
+  for(j=20;j<17000;j+=20){
     fill(color(random(255), random(255),     random(255)));
     circle(i, j, random(20)); 
        // square(i, j, random(20)); 
